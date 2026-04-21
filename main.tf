d2534a2e372090d4fe9b0fc5fb7f5a08a3353cda
hemanthrajgandham <33430687+hemanthrajgandham@users.noreply.github.com>
2026-04-21 15:37:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -151,7 +151,46 @@ data "cloudinit_config" "monitoring" {
                 timeout: 5s
           EOT
         }
-      ]
+{
+  path = "/etc/prometheus/prometheus.yml"
+  content = <<-EOT
+    global:
+      scrape_interval: 15s
+
+    rule_files:
+      - /etc/prometheus/rules.yml
+
+    scrape_configs:
+      - job_name: 'prometheus'
+        static_configs:
+          - targets: ['localhost:9090']
+
+      - job_name: 'node'
+        static_configs:
+          - targets: ['localhost:9100']
+
+      - job_name: 'blackbox'
+        metrics_path: /probe
+        params:
+          module: [http_2xx]
+
+        static_configs:
+          - targets:
+            - https://www.google.com
+            - https://developer.fiserv.com
+
+        relabel_configs:
+          - source_labels: [__address__]
+            target_label: __param_target
+
+          - source_labels: [__param_target]
+            target_label: instance
+
+          - target_label: __address__
+            replacement: localhost:9115
+  EOT
+}
+]
 
       runcmd = [
         "systemctl enable prometheus",
